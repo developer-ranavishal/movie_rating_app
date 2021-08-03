3c0bd33026d65d074199e946bff7e85f39edbdeb
Vishal Rana <vishalrana9038@gmail.com>
2021-08-03 16:21:49 +0530

    changed TAG

--- app/src/main/java/com/example/imdb_app/constant/Constant.kt
+++ app/src/main/java/com/example/imdb_app/constant/Constant.kt
@@ -4,4 +4,4 @@ package com.example.imdb_app.constant
 //https://api.themoviedb.org/3/movie/popular?api_key=3380e7d7ab09793624ad74578cf4fd78
 const val BASE_URL="https://api.themoviedb.org/3/"
 const val API_KEY="3380e7d7ab09793624ad74578cf4fd78"
-const val NETWORK_TAG="MovieViewModel"
+const val TAGN="MovieViewModel"
