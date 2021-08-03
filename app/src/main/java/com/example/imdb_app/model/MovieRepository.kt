4879265cbb1c6c4c31b4a444763e4bc89f8693b9
Vishal Rana <vishalrana9038@gmail.com>
2021-08-03 16:22:47 +0530

    called popular movie api in flow.

--- app/src/main/java/com/example/imdb_app/model/MovieRepository.kt
+++ app/src/main/java/com/example/imdb_app/model/MovieRepository.kt
@@ -1,9 +1,36 @@
 package com.example.imdb_app.model
 
 import com.example.imdb_app.network.MovieApi
+import com.example.imdb_app.network.Results
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 
-class MovieRepository @Inject constructor(movieApi: MovieApi) {
+class MovieRepository @Inject constructor(_movieApi: MovieApi) {
+
+    private val movieApi = _movieApi
+    fun getPopularMovies(): Flow<List<Results>> = flow {
+        val getResponse = movieApi.getMovies()
+        emit(getResponse.body()!!.results)
+    }.flowOn(Dispatchers.IO)
 
 }
+
+/**
+ *     private fun getMovies(movieApi: MovieApi) {
+viewModelScope.launch {
+val movieResponse=movieApi.getMovies()
+val responseBody=movieResponse.body()
+Log.d(NETWORK_TAG, "total page are  ${responseBody?.total_pages}")
+try {
+if (responseBody!=null) {
+Log.d(NETWORK_TAG, "movie are ${responseBody.results} ")
+mPopularMovie.value = responseBody.results
+}
+}catch (t : Throwable){
+Log.d(NETWORK_TAG, "error is ${t.localizedMessage}")
+}
+ */
